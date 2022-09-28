81dc78a3f98b793c38a288d4b09e2dd2678a2027
PorridgePi <kunbao2006@126.com>
2022-09-28 19:42:05 +0800

    bootstrap: lsd: Initial commit

---
+++ .config/yadm/bootstrap.d/lsd.sh
@@ -0,0 +1,21 @@
+if [[ "$OSNAME" == "Linux" ]]; then
+    # Linux
+    if [[ "DISTRO_TYPE" == "debian" ]]; then
+        # Debian-based
+        URL=$(curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep $(uname -m) | grep -v musl)
+        curl -o /temp/lsd.deb "$URL"
+        sudo dpkg -i /temp/lsd.deb
+    else
+        # Unknown distro
+        echo 'ERROR: Unknown distro'
+        echo 'Exiting...'
+        exit 1
+elif [[ "$OSNAME" == "macOS" ]]; then
+    # macOS
+    brew install lsd
+else
+    # Unknown
+    echo 'ERROR: Unknown OS'
+    echo 'Exiting...'
+    exit 1
+fi
