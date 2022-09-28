38568dba85350541184353b8c22ee0512a3aea2d
PorridgePi <kunbao2006@126.com>
2022-09-28 19:50:53 +0800

    bootstrap: lsd: feat: Add check if lsd is installed

--- .config/yadm/bootstrap.d/lsd.sh
+++ .config/yadm/bootstrap.d/lsd.sh
@@ -1,22 +1,28 @@
-if [[ "$OSNAME" == "Linux" ]]; then
-    # Linux
-    if [[ "DISTRO_TYPE" == "debian" ]]; then
-        # Debian-based
-        URL=$(curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep $(uname -m) | grep -v musl)
-        curl -o /temp/lsd.deb "$URL"
-        sudo dpkg -i /temp/lsd.deb
+if ! command -v lsd &> /dev/null; then
+    echo 'Installing lsd'
+
+    if [[ "$OSNAME" == "Linux" ]]; then
+        # Linux
+        if [[ "DISTRO_TYPE" == "debian" ]]; then
+            # Debian-based
+            URL=$(curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep $(uname -m) | grep -v musl)
+            curl -o /temp/lsd.deb "$URL"
+            sudo dpkg -i /temp/lsd.deb
+        else
+            # Unknown distro
+            echo 'ERROR: Unknown distro'
+            echo 'Exiting...'
+            exit 1
+        fi
+    elif [[ "$OSNAME" == "macOS" ]]; then
+        # macOS
+        brew install lsd
     else
-        # Unknown distro
-        echo 'ERROR: Unknown distro'
+        # Unknown
+        echo 'ERROR: Unknown OS'
         echo 'Exiting...'
         exit 1
     fi
-elif [[ "$OSNAME" == "macOS" ]]; then
-    # macOS
-    brew install lsd
-else
-    # Unknown
-    echo 'ERROR: Unknown OS'
-    echo 'Exiting...'
-    exit 1
 fi
+
+echo 'lsd is installed'
