0eaf934c1520e6072fb98b29fa9f2509e90b7c7a
PorridgePi <kunbao2006@126.com>
2022-09-28 20:00:22 +0800

    bootstrap: lsd: fix: Add missing dollar sign

--- .config/yadm/bootstrap.d/lsd.sh
+++ .config/yadm/bootstrap.d/lsd.sh
@@ -3,7 +3,7 @@ if ! command -v lsd &> /dev/null; then
 
     if [[ "$OSNAME" == "Linux" ]]; then
         # Linux
-        if [[ "DISTRO_TYPE" == "debian" ]]; then
+        if [[ "$DISTRO_TYPE" == "debian" ]]; then
             # Debian-based
             URL=$(curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep $(uname -m) | grep -v musl)
             curl -o /temp/lsd.deb "$URL"
