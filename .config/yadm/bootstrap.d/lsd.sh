a90c3b4ace23e913cb1b9bd8eeebd6c18379fa3c
PorridgePi <kunbao2006@126.com>
2022-09-28 20:00:59 +0800

    bootstrap: lsd: fix: Change ARCH from aarch64 to arm64

--- .config/yadm/bootstrap.d/lsd.sh
+++ .config/yadm/bootstrap.d/lsd.sh
@@ -5,7 +5,10 @@ if ! command -v lsd &> /dev/null; then
         # Linux
         if [[ "$DISTRO_TYPE" == "debian" ]]; then
             # Debian-based
-            URL=$(curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep $(uname -m) | grep -v musl)
+            if [[ "$ARCH" == "aarch64" ]]; then
+                ARCH=arm64
+            fi
+            URL=$(curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep $ARCH | grep -v musl)
             curl -o /temp/lsd.deb "$URL"
             sudo dpkg -i /temp/lsd.deb
         else
